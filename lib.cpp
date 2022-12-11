dc049239479ebad3cd01e13f39717fca4ab98dab
AuroraFranceschini <115407804+AuroraFranceschini@users.noreply.github.com>
2022-12-11 12:02:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
+float maggiore (float numeri [], int a) {
+for (int i=0; i<a; ++i) {
+    if (numeri [0] > numeri [i]){
 
+    }else {
+        numeri [0] = numeri [i];
+    }
+}
+return numeri [0];
+}
